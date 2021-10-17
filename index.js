bc629bc779e5449d1c8b8d5dcb93cbca3a95367a
EricFooSD <fooshida@gmail.com>
2021-10-17 21:46:57 +0800

    modifu server

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ const whenIncomingRequest = (request, response) => {
   var filePath = '.' + request.url;
 
   readFile(filePath, (error, content) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.writeHead(200, {
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.css': 'text/css',
+      '.json': 'application/json',
+      '.png': 'image/png',
+      '.jpg': 'image/jpg',
+      '.gif': 'image/gif',
+      '.svg': 'image/svg+xml',
+      '.wav': 'audio/wav',
+      '.mp4': 'video/mp4',
+      '.woff': 'application/font-woff',
+      '.ttf': 'application/font-ttf',
+      '.eot': 'application/vnd.ms-fontobject',
+      '.otf': 'application/font-otf',
+      '.wasm': 'application/wasm',
+    });
     response.end(content, 'utf-8');
   });
 };
